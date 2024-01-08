16d2b06f3ad2c6d0ab2e9c5b55549c2216da8496
Diego0024 <diegovtellez10@gmail.com>
2024-01-08 16:23:06 -0600

    Cambio1

---
+++ variables.js
@@ -0,0 +1,52 @@
+var edadDiego = '24';
+console.log("La edad de Diego es " 
++ edadDiego);
+
+let diegoDateOfBirth = 1999;
+console.log("Diego was born in " 
++ diegoDateOfBirth);
+
+let suma = edadDiego + diegoDateOfBirth;
+console.log("La suma de la edad de Diego y el año de nacimiento es " + suma)
+
+let postreFavorito = 'Helado';
+
+var movieFav = "Toy story";
+
+// Variables de tipo string, comillas dobles o simples
+
+let miNombreEsDiego = false;
+
+var amIAlive = true;
+
+// Variables de tipo nulo (null)
+let datoVacio = null;
+let datoVacio1 = "";
+
+//Variables de tipo array
+
+let coleccionDeCoches = ["Porsche", "Mercedes-Benz", "Ferrari", "Nissan"]
+
+let frutas = ["Manzana", "Platano", "Mango"];
+
+let chuches = ["chips moradas", "takis verdes", "huevito kinder", "churritos con salsa"];
+// var
+var nombre = "Marcus";
+console.log(nombre)
+
+if (true){
+    var nombre = "Dominic";
+    console.log(nombre);
+}
+ // let
+console.log(nombre);
+
+var edad = 45;
+console.log(edad);
+
+if (true){
+    let edad = 40;
+    console.log(edad)
+}
+
+console.log(edad);
